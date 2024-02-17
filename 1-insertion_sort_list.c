094e7e6447857e9dc7c9ee1002043c80c33cb35b
Kofiad <kofiadaba@gmail.com>
2024-02-17 15:37:30 +0000

    started task 1

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,11 @@
+#include "sort.h"
+
+/**
+ * insertion_sort_list - sorts a doubly linked list of integers in ascending order
+ * using the Insertion sort algorithm
+ * @list: doubly linked list of integers
+ */
+void insertion_sort_list(listint_t **list)
+{
+	
+}
