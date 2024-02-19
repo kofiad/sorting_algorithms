6423bf4f18e82b0413ffe7b08be97e65f1e80660
kofiad <kofiadaba@gmail.com>
2024-02-19 12:58:21 +0000

    work in progress

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -74,3 +74,17 @@ void lomuto_sort(int *array, size_t size, int left, int right)
 		lomuto_sort(array, size, part + 1, right);
 	}
 }
+
+/**
+ * swap_ints - Swap two integers in an array.
+ * @a: The first integer to swap.
+ * @b: The second integer to swap.
+ */
+void swap_ints(int *a, int *b)
+{
+	int tmp;
+
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
